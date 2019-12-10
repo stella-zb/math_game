993738966fc0932227e1373f4d8163e25d7e68f3
stella-zb <upperdaystudio@gmail.com>
2019-12-09 22:11:37 -0800

    add player id, create lose method to update the lives if lose

--- player.rb
+++ player.rb
@@ -1,8 +1,13 @@
 class Player
-  attr_accessor :name, :lives
+  attr_accessor :id, :name, :lives
 
-  def initialize (name) 
+  def initialize(id, name, lives) 
+    @id = id
     @name = name
-    @lives = 3
+    @lives = lives
+  end
+
+  def lose
+    @lives = @lives - 1
   end
 end
