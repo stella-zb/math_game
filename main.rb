0a298b7572faf7fa4cb759046245c67288b16f0a
stella-zb <upperdaystudio@gmail.com>
2019-12-09 22:09:57 -0800

    init players list, cycle the game for each player to take turn

--- main.rb
+++ main.rb
@@ -1,6 +1,15 @@
 require './player'
 require './game'
 
-game = Game.new
-game.question
-game.result
+# init players
+players = [Player.new("P1", "Player1", 3), Player.new("P2", "Player2", 3)]
+
+# cycle the player to play the game
+players.cycle { |player|
+  game = Game.new
+  game.question(player)
+  game.result(player)
+}
+  
+  
+  
