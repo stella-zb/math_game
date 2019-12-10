21eb8cb5494e26e154722ebc111e2713a0694cd2
stella-zb <upperdaystudio@gmail.com>
2019-12-10 00:40:58 -0800

    set the condition if lives equal to 0, terminate the game

--- main.rb
+++ main.rb
@@ -6,10 +6,14 @@ players = [Player.new("P1", "Player1", 3), Player.new("P2", "Player2", 3)]
 
 # cycle the player to play the game
 players.cycle { |player|
-  game = Game.new
-  game.question(player)
-  game.result(player)
+  if player.lives == 0
+    puts "----- GAME OVER ------\n Good bye!"
+    break
+  else
+    game = Game.new
+    game.question(player)
+    game.result(player)
+  end
 }
-  
-  
-  
+
+
